33a07bee07dcd75adbfc22fe8ca3b4e892069189
Robert Riemann <robert@riemann.cc>
2015-07-09 18:48:02 +0200

    wip

--- Gemfile
+++ Gemfile
@@ -1,34 +1,34 @@
-source 'https://rubygems.org'
-# Core
-gem "middleman", "3.3.7"
+# If you have OpenSSL installed, we recommend updating
+# the following line to use 'https'
+source 'http://rubygems.org'
 
-# Assets
-gem "bootstrap-sass", "~> 3.3.4", require: false
-gem "font-awesome-middleman", "~> 4.2.3"
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
-source 'https://rails-assets.org' do  
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+# Middleman Gems
+# gem 'middleman', '~> 4.0.0.beta.2'
+gem 'middleman', :git => 'https://github.com/middleman/middleman.git', :branch => 'master'
+gem 'middleman-livereload'
+
+
+gem 'middleman-jquery'
+gem 'font-awesome-sass', require: false
+
+gem 'slim'
+
+source 'https://rails-assets.org' do
   gem 'rails-assets-mapbox.js', '~> 2.1.9', require: false
   
   gem 'rails-assets-jquery.scrollex', require: false
   gem 'rails-assets-skel', '~> 3.0.0.dev', require: false
   gem 'rails-assets-html5shiv', '~> 3.6.2', require: false
   gem 'rails-assets-respond', require: false
+  
+  gem 'rails-assets-jquery.easing', '~> 1.3.1', require: false
+  gem 'rails-assets-Hover', '~> 1.0.9', require: false
+  gem 'rails-assets-formstone', '~> 0.6.1', require: false
+  gem 'rails-assets-jquery.stellar', '~> 0.6.2', require: false
 end
-
-gem "middleman-favicon-maker", "~> 3.7"
-
-# Templating
-gem "slim", "~> 3.0.1"
-# gem "middleman-autoprefixer", "~> 2.3.0"
-
-# Optimization & minification
-gem "middleman-jquery", "~> 2.1.1"
-gem "middleman-minify-html", "~> 3.4.0"
-gem "middleman-imageoptim", "~> 0.1.4"
-gem "builder", "~> 3.2.2"
-gem "middleman-sitemap", "~> 0.0.13"
-gem "middleman-dotenv"
-
-gem "middleman-livereload", "~> 3.3.4"
-gem "middleman-deploy", "~> 1.0.0"
-gem "oj", "~> 2.11.1"
--- config.rb
+++ config.rb
@@ -1,76 +1,40 @@
-# ========================================================================
-# Site settings
-# ========================================================================
-set :site_title,            "Visionary's Club"
-set :site_description,      "Website of the Visionary's Club"
-set :site_url_production,   "visionarys.club"
-set :site_url_development,  "0.0.0.0:4567"
 set :css_dir,               'css'
 set :js_dir,                'js'
 set :images_dir,            'img'
 set :fonts_dir,             'fonts'
 
-# Sitemap XML
-require "builder"
-page "/sitemap.xml", :layout => false
-
 # Slim template engine
-require "slim"
+require 'slim'
 
-# necessary for copyright in footer
-require "date"
+# explicit require of sass as suggested by 'tilt'
+require 'sass'
 
-# Internationalization
-activate :i18n
-
-# get api keys from ".env" file
-activate :dotenv
-
-# Use relative URLs
-activate :relative_assets
-
-# Pretty URLs
-activate :directory_indexes
-set :trailing_slash, true
-
-# Autoprefixer
-# activate :autoprefixer do |config|
-#   config.browsers = ['last 2 versions', 'Explorer >= 9']
-#   config.cascade  = false
-#   config.inline   = false
-# end
-
-# ========================================================================
+###
 # Page options, layouts, aliases and proxies
-# ========================================================================
+###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html",
-# :locals => {:which_fake_page => "Rendering a fake page with a local
-# variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
-# ========================================================================
+###
 # Helpers
-# ========================================================================
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+###
 
 # Reload the browser automatically whenever files change
-activate :livereload
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -79,72 +43,17 @@ activate :livereload
 #   end
 # end
 
-# ========================================================================
-# Development-specific configuration
-# ========================================================================
-configure :development do
-  set :site_url, "#{site_url_development}"
-  set :sass, :sourcemap => :inline, style: :nested
-
-  set :slim, {
-    # :format  => :html5,
-    # :attr_wrapper => '"',
-    # :attr_delims => {'(' => ')', '[' => ']'}, # removed '{' => '}' from default
-    :indent => '    ',
-    :pretty => true,
-    :sort_attrs => false
-  }
-end
-
-# ========================================================================
 # Build-specific configuration
-# ========================================================================
 configure :build do
-  set :site_url, "#{site_url_production}"
-  set :sass, style: :compressed
+  # Minify CSS on build
+  activate :minify_css
 
-  # activate :minify_css
-  activate :minify_html
+  # Minify Javascript on build
   activate :minify_javascript
-  activate :gzip
-
-  # Enable cache buster
-  activate :asset_hash, :exts => ['.css', '.png', '.jpg', '.gif', '.js']
-
-  # Ignore files/dir during build process
-  ignore "favicon_template.png"
-  ignore "sitemap.yml"
-
-  # Create favicon and device-specific icons
-  activate :favicon_maker, :icons => {
-    "favicon_template.png" => [
-      { icon: "apple-touch-icon-152x152-precomposed.png" },
-      { icon: "apple-touch-icon-144x144-precomposed.png" },
-      { icon: "apple-touch-icon-120x120-precomposed.png" },
-      { icon: "apple-touch-icon-114x114-precomposed.png" },
-      { icon: "apple-touch-icon-76x76-precomposed.png" },
-      { icon: "apple-touch-icon-72x72-precomposed.png" },
-      { icon: "apple-touch-icon-60x60-precomposed.png" },
-      { icon: "apple-touch-icon-57x57-precomposed.png" },
-      { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
-      { icon: "apple-touch-icon.png", size: "57x57" },
-      { icon: "favicon-196x196.png" },
-      { icon: "favicon-160x160.png" },
-      { icon: "favicon-96x96.png" },
-      { icon: "favicon-32x32.png" },
-      { icon: "favicon-16x16.png" },
-      { icon: "favicon.png", size: "16x16" },
-      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
-      { icon: "mstile-144x144", format: "png" },
-    ]
-  }
 end
 
-activate :deploy do |deploy|
-  deploy.method = :rsync
-  deploy.host   = "rriemann.rigel.uberspace.de"
-  deploy.path   = "/var/www/virtual/rriemann/visionarys.club"
-  # deploy.build_before = true # default: false
-  # deploy.method   = :ftp
-  deploy.clean = true # remove orphaned files on remote host, default: false
+# work-around to remove copies of font-awesome files. Where are they pulled in?
+after_build do |builder|
+  build_dir = config[:build_dir]
+  Dir.glob(build_dir + '/fonts/*wesome*').each { |f| File.delete(f) if File.file? f }
 end
